d04617230a6f0c586d46978c75c609dc517dbb78
hanin303 <benjemaahanin@gmail.com>
2021-11-25 00:48:04 +0100

    Full Navigation with named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,20 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Provide a function to handle named routes.
-      // Use this function to identify the named
-      // route being pushed, and create the correct
-      // Screen.
+      routes: {
+        ExtractArgumentsScreen.routeName: (context) =>
+        const ExtractArgumentsScreen(),
+      },
       onGenerateRoute: (settings) {
-        // If you push the PassArguments route
+
         if (settings.name == PassArgumentsScreen.routeName) {
-          // Cast the arguments to the correct
-          // type: ScreenArguments.
           final args = settings.arguments as ScreenArguments;
 
-          // Then, extract the required data from
-          // the arguments and pass the data to the
-          // correct screen.
           return MaterialPageRoute(
             builder: (context) {
               return PassArgumentsScreen(
@@ -33,17 +26,14 @@ class MyApp extends StatelessWidget {
             },
           );
         }
-        // The code only supports
-        // PassArgumentsScreen.routeName right now.
-        // Other values need to be implemented if we
-        // add them. The assertion here will help remind
-        // us of that higher up in the call stack, since
-        // this assertion would otherwise fire somewhere
-        // in the framework.
         assert(false, 'Need to implement ${settings.name}');
         return null;
       },
-    )
+      title: 'Navigation with Arguments',
+      home: const HomeScreen(),
+    );
+  }
+}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -58,15 +48,8 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // A button that navigates to a named route.
-            // The named route extracts the arguments
-            // by itself.
             ElevatedButton(
               onPressed: () {
-                // When the user taps the button,
-                // navigate to a named route and
-                // provide the arguments as an optional
-                // parameter.
                 Navigator.pushNamed(
                   context,
                   ExtractArgumentsScreen.routeName,
@@ -78,15 +61,8 @@ class HomeScreen extends StatelessWidget {
               },
               child: const Text('Navigate to screen that extracts arguments'),
             ),
-            // A button that navigates to a named route.
-            // For this route, extract the arguments in
-            // the onGenerateRoute function and pass them
-            // to the screen.
             ElevatedButton(
               onPressed: () {
-                // When the user taps the button, navigate
-                // to a named route and provide the arguments
-                // as an optional parameter.
                 Navigator.pushNamed(
                   context,
                   PassArgumentsScreen.routeName,
@@ -106,70 +82,50 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-  // A Widget that extracts the necessary arguments from
-// the ModalRoute.
-  class ExtractArgumentsScreen extends StatelessWidget {
+class ExtractArgumentsScreen extends StatelessWidget {
   const ExtractArgumentsScreen({Key? key}) : super(key: key);
 
   static const routeName = '/extractArguments';
 
   @override
   Widget build(BuildContext context) {
-  // Extract the arguments from the current ModalRoute
-  // settings and cast them as ScreenArguments.
-  final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
-
-  return Scaffold(
-  appBar: AppBar(
-  title: Text(args.title),
-  ),
-  body: Center(
-  child: Text(args.message),
-  ),
-  );
-  }
-  }
-
-  @override
-  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
 
     return Scaffold(
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title: Text(args.title),
       ),
       body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Text(args.message),
       ),
     );
   }
 }
+
+class PassArgumentsScreen extends StatelessWidget {
+  static const routeName = '/passArguments';
+
+  final String title;
+  final String message;
+  const PassArgumentsScreen({
+    Key? key,
+    required this.title,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(message),
+      ),
+    );
+  }
+}
+
 class ScreenArguments {
   final String title;
   final String message;
